1efd6676b8c7b929d580c99bcb8f8f40f73a814d
natscreates <natalie.avellanet@gmail.com>
2014-12-17 11:08:50 +0000

    first commit

---
+++ client/views/matches/matchList.js
@@ -0,0 +1,23 @@
+var matchData = [
+  {
+    service: 'Dorm to Apartment',
+    date: 'Available: 08/16/15',
+  },
+  {
+    service: 'Dorm to Apartment',
+    date: 'Available: 01/20/15',
+  },
+  {
+    service: 'Apartment to Apartment',
+    date: 'Available: 02/25/15',
+  },
+  {
+    service: 'Apartment to Apartment',
+    date: 'Available: 09/01/15',
+  }
+];
+
+Template.matchList.helpers({
+  matches: matchData
+  
+});
